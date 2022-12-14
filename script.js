0a5f07454e3f5edda12bd98179789afec1d34843
Khalid Waleed <khalidwaleed0@outlook.com>
2022-12-14 19:58:00 +0200

    Initial local storage support
    gets and adds projects and tasks successfully.

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-import { addListeners } from '/scripts/dom-manipulator.js';
-addListeners();
+import { addListeners, addProjects, addTasks } from "/scripts/dom-manipulator.js";
+
+if (storageAvailable("localStorage")) {
+	let projects = JSON.parse(localStorage.getItem("projects")) ?? [];
+	let notes = JSON.parse(localStorage.getItem("notes")) ?? [];
+	let tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
+	addProjects(projects);
+	addTasks(tasks);
+	addListeners();
+}
+
+function storageAvailable(type) {
+	let storage;
+	try {
+		storage = window[type];
+		const x = "__storage_test__";
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	} catch (e) {
+		return (
+			e instanceof DOMException &&
+			// everything except Firefox
+			(e.code === 22 ||
+				// Firefox
+				e.code === 1014 ||
+				// test name field too, because code might not be present
+				// everything except Firefox
+				e.name === "QuotaExceededError" ||
+				// Firefox
+				e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+			// acknowledge QuotaExceededError only if there's something already stored
+			storage &&
+			storage.length !== 0
+		);
+	}
+}
--- scripts/dom-manipulator.js
+++ scripts/dom-manipulator.js
@@ -1,17 +1,36 @@
 const todoFormContent = `<input class="title" type="text" placeholder="Todo Title:" maxlength="36" />
                         <textarea class="details" placeholder="Details:"></textarea>
                         <input class="date" type="date" />
-                        <button type="button">Add</button>`;
+                        <button class="btn-add-todo another-class" type="button">Add</button>`;
 
 const noteFormContent = `<input class="title" type="text" placeholder="Note Title:" maxlength="36" />
                         <textarea class="details" placeholder="Details:"></textarea>
-                        <button type="button">Add</button>`;
+                        <button class="btn-add-note" type="button">Add</button>`;
 
 const projectFormContent = `<input class="title" type="text" placeholder="Project Title:" maxlength="36" />
-                            <button type="button">Add</button>`;
+                            <button class="btn-add-project" type="button">Add</button>`;
 
-const mainSidebarButtons = document.querySelectorAll(".side-bar * span:first-child");
 const formSidebarButtons = document.querySelectorAll(".form-container .side-bar div span");
+
+let projects = JSON.parse(localStorage.getItem("projects")) ?? [];
+let notes = JSON.parse(localStorage.getItem("notes")) ?? [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
+
+class Note {
+	constructor(title, details) {
+		this.title = title;
+		this.details = details;
+	}
+}
+
+class Task {
+	constructor(title, details, date, project) {
+		this.title = title;
+		this.details = details;
+		this.date = date;
+		this.project = project;
+	}
+}
 
 export function addListeners() {
 	addIconListeners();
@@ -37,6 +56,7 @@ function addIconListeners() {
 }
 
 function addMainSidebarListeners() {
+	let mainSidebarButtons = document.querySelectorAll("main .side-bar * span:first-child");
 	mainSidebarButtons.forEach((item) => {
 		item.addEventListener("click", (e) => {
 			mainSidebarButtons.forEach((e1) => {
@@ -55,9 +75,9 @@ function addNewFormListeners() {
 			document.querySelector(".new-form-container form").removeAttribute("closed");
 		}, 400);
 	};
-	document.querySelector(".form-container .todo").onclick = showTodoForm;
-	document.querySelector(".form-container .project").onclick = showProjectForm;
-	document.querySelector(".form-container .note").onclick = showNoteForm;
+	document.querySelector(".new-form-container .todo").onclick = showTodoForm;
+	document.querySelector(".new-form-container .project").onclick = showProjectForm;
+	document.querySelector(".new-form-container .note").onclick = showNoteForm;
 }
 
 function addNewFormSidebarListeners() {
@@ -83,12 +103,72 @@ function addEditFormListeners() {
 
 function showTodoForm() {
 	document.querySelector("form .content").innerHTML = todoFormContent;
+	document.querySelector(".new-form-container form button").onclick = submitForm;
 }
 
 function showProjectForm() {
 	document.querySelector("form .content").innerHTML = projectFormContent;
+	document.querySelector(".new-form-container form button").onclick = submitForm;
 }
 
 function showNoteForm() {
 	document.querySelector("form .content").innerHTML = noteFormContent;
+	document.querySelector(".new-form-container form button").onclick = submitForm;
+}
+
+function submitForm(e) {
+	let title = document.querySelector(".new-form-container form input.title").value;
+	if (e.target.className.includes("project")) {
+		projects.push(title);
+		localStorage.setItem("projects", JSON.stringify(projects));
+		alert(projects);
+		addProjects(projects.slice(-1));
+		addMainSidebarListeners();
+	} else if (e.target.className.includes("note")) {
+		let details = document.querySelector(".new-form-container form textarea.details").value;
+		notes.push(new Note(title, details));
+		localStorage.setItem("notes", JSON.stringify(notes));
+		alert(JSON.stringify(notes));
+	} else if (e.target.className.includes("todo")) {
+		let details = document.querySelector(".new-form-container form textarea.details").value;
+		let date = document.querySelector(".new-form-container form input.date").value;
+		let project = document.querySelector("#projects-container span[active]")?.textContent ?? null;
+		tasks.push(new Task(title, details, date, project));
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+		alert(JSON.stringify(tasks));
+		addTasks(tasks.slice(-1));
+		addIconListeners();
+	}
+}
+
+export function addProjects(projects) {
+	for (const project of projects) {
+		document.getElementById("projects-container").innerHTML += `<div class="project">
+																	<span>${project}</span>
+																	<span class="counter">4</span>
+																	</div>`;
+	}
+}
+
+export function addTasks(tasks) {
+	for (const task of tasks) {
+		document.querySelector(".main-content").innerHTML += `<div class="task-card">
+		<input type="checkbox" class="checkbox" />
+		<p class="task-name">${task["title"]}</p>
+		<div class="task-actions-container">
+			<button class="task-details">Details</button>
+			<p class="task-date">${task["date"]}</p>
+			<svg class="edit-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+				<path
+					d="M373.1 24.97C401.2-3.147 446.8-3.147 474.9 24.97L487 37.09C515.1 65.21 515.1 110.8 487 138.9L289.8 336.2C281.1 344.8 270.4 351.1 258.6 354.5L158.6 383.1C150.2 385.5 141.2 383.1 135 376.1C128.9 370.8 126.5 361.8 128.9 353.4L157.5 253.4C160.9 241.6 167.2 230.9 175.8 222.2L373.1 24.97zM440.1 58.91C431.6 49.54 416.4 49.54 407 58.91L377.9 88L424 134.1L453.1 104.1C462.5 95.6 462.5 80.4 453.1 71.03L440.1 58.91zM203.7 266.6L186.9 325.1L245.4 308.3C249.4 307.2 252.9 305.1 255.8 302.2L390.1 168L344 121.9L209.8 256.2C206.9 259.1 204.8 262.6 203.7 266.6zM200 64C213.3 64 224 74.75 224 88C224 101.3 213.3 112 200 112H88C65.91 112 48 129.9 48 152V424C48 446.1 65.91 464 88 464H360C382.1 464 400 446.1 400 424V312C400 298.7 410.7 288 424 288C437.3 288 448 298.7 448 312V424C448 472.6 408.6 512 360 512H88C39.4 512 0 472.6 0 424V152C0 103.4 39.4 64 88 64H200z"
+				/>
+			</svg>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+				<path
+					d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"
+				/>
+			</svg>
+		</div>
+	</div>`;
+	}
 }
