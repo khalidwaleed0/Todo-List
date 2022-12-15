c05596f16cb15190542554ca3b323ca3860388f8
Khalid Waleed <khalidwaleed0@outlook.com>
2022-12-15 17:48:40 +0200

    Added support for editing cards

--- scripts/dom-manipulator.js
+++ scripts/dom-manipulator.js
@@ -15,6 +15,8 @@ const formSidebarButtons = document.querySelectorAll(".form-container .side-bar 
 let projects = JSON.parse(localStorage.getItem("projects")) ?? [];
 let notes = JSON.parse(localStorage.getItem("notes")) ?? [];
 let tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
+let selectedTaskIndex = 0;
+let selectedCard;
 
 class Note {
 	constructor(title, details) {
@@ -53,8 +55,28 @@ function addIconListeners() {
 
 function addCardsListeners() {
 	document.querySelectorAll("svg.edit-icon").forEach((item) => {
-		item.onclick = () => {
+		item.onclick = (e) => {
+			selectedCard = e.target.parentElement.parentElement;
+			let taskName = item.parentElement.parentElement.querySelector(".task-name").textContent;
+			for (let i = 0; i < tasks.length; i++) {
+				if (tasks[i].title === taskName) {
+					selectedTaskIndex = i;
+					break;
+				}
+			}
 			document.querySelector(".edit-form-container").toggleAttribute("active");
+			document.querySelector(".edit-form-container input.title").value = tasks[selectedTaskIndex].title;
+			document.querySelector(".edit-form-container textarea.details").value = tasks[selectedTaskIndex].details;
+			document.querySelector(".edit-form-container input.date").value = tasks[selectedTaskIndex].date;
+		};
+	});
+	document.querySelectorAll(".remove-icon").forEach((item) => {
+		item.onclick = () => {
+			let taskName = item.parentElement.parentElement.querySelector(".task-name").textContent;
+			alert(taskName);
+			tasks = tasks.filter((task) => task["title"] !== taskName);
+			item.parentElement.parentElement.remove();
+			localStorage.setItem("tasks", JSON.stringify(tasks));
 		};
 	});
 }
@@ -107,13 +129,28 @@ function addNewFormSidebarListeners() {
 	});
 }
 
+function closeEditForm() {
+	document.querySelector(".edit-form-container form").setAttribute("closed", "");
+	setTimeout(() => {
+		document.querySelector(".edit-form-container").toggleAttribute("active");
+		document.querySelector(".edit-form-container form").removeAttribute("closed");
+	}, 400);
+}
+
 function addEditFormListeners() {
-	document.querySelector(".edit-form-container .btn-close").onclick = () => {
-		document.querySelector(".edit-form-container form").setAttribute("closed", "");
-		setTimeout(() => {
-			document.querySelector(".edit-form-container").toggleAttribute("active");
-			document.querySelector(".edit-form-container form").removeAttribute("closed");
-		}, 400);
+	document.querySelector(".edit-form-container .btn-close").onclick = closeEditForm;
+	document.querySelector(".edit-form-container button").onclick = (e) => {
+		let title = document.querySelector(".edit-form-container input.title").value;
+		let details = document.querySelector(".edit-form-container textarea.details").value;
+		let date = document.querySelector(".edit-form-container input.date").value;
+		selectedCard.querySelector(".task-name").textContent = title;
+		selectedCard.querySelector(".task-date").textContent = date;
+		tasks[selectedTaskIndex].title = title;
+		tasks[selectedTaskIndex].details = details;
+		tasks[selectedTaskIndex].date = date;
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+		document.querySelector(".edit-form-container form").reset();
+		closeEditForm();
 	};
 }
 
@@ -155,6 +192,7 @@ function submitForm(e) {
 		addTasks(tasks.slice(-1));
 		addIconListeners();
 	}
+	document.querySelector(".new-form-container form").reset();
 }
 
 export function addProjects(projects) {
@@ -179,7 +217,7 @@ export function addTasks(tasks) {
 					d="M373.1 24.97C401.2-3.147 446.8-3.147 474.9 24.97L487 37.09C515.1 65.21 515.1 110.8 487 138.9L289.8 336.2C281.1 344.8 270.4 351.1 258.6 354.5L158.6 383.1C150.2 385.5 141.2 383.1 135 376.1C128.9 370.8 126.5 361.8 128.9 353.4L157.5 253.4C160.9 241.6 167.2 230.9 175.8 222.2L373.1 24.97zM440.1 58.91C431.6 49.54 416.4 49.54 407 58.91L377.9 88L424 134.1L453.1 104.1C462.5 95.6 462.5 80.4 453.1 71.03L440.1 58.91zM203.7 266.6L186.9 325.1L245.4 308.3C249.4 307.2 252.9 305.1 255.8 302.2L390.1 168L344 121.9L209.8 256.2C206.9 259.1 204.8 262.6 203.7 266.6zM200 64C213.3 64 224 74.75 224 88C224 101.3 213.3 112 200 112H88C65.91 112 48 129.9 48 152V424C48 446.1 65.91 464 88 464H360C382.1 464 400 446.1 400 424V312C400 298.7 410.7 288 424 288C437.3 288 448 298.7 448 312V424C448 472.6 408.6 512 360 512H88C39.4 512 0 472.6 0 424V152C0 103.4 39.4 64 88 64H200z"
 				/>
 			</svg>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+			<svg class="remove-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
 				<path
 					d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"
 				/>
